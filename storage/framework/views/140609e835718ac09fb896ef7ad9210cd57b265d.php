af574d9eb495ee21a8bb50740897add5f517c3eb
naveed.zahoor <naveed.zahoor@team.esipick.com>
2023-05-13 02:46:15 +0500

    git cache updated

--- storage/framework/views/140609e835718ac09fb896ef7ad9210cd57b265d.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" />
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonymous"></script><?php /**PATH /var/www/html/resources/views/vendor/seo/admin/layouts/style.blade.php ENDPATH**/ ?>
--- storage/framework/views/4f32440f8fc35a740b10162063f3b43083e6a3d3.php
+++
@@ -1,9 +0,0 @@
-<form onsubmit="return confirm('Are you sure you want to delete?')"
-      action="<?php echo e(isset($route)?$route:''); ?>" method="post" style="display: inline">
-    <?php echo e(csrf_field()); ?>
-
-    <?php echo e(method_field('DELETE')); ?>
-
-    <button type="submit" class="btn btn-default cursor-pointer  btn-sm"><i
-                class="text-danger fa fa-remove"></i></button>
-</form><?php /**PATH /var/www/html/packages/laravel-seo-tools-master/SEO/src/Providers/../Resources/views/admin/forms/destroy.blade.php ENDPATH**/ ?>
