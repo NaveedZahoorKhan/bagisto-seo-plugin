f2e841c265ddbff609b3e348f0ef041443532fb5
naveed.zahoor <naveed.zahoor@team.esipick.com>
2023-06-13 01:37:59 +0500

    settings route updated

--- packages/laravel-seo-tools-master/SEO/src/Http/Controllers/Admin/SettingController.php
+++ packages/laravel-seo-tools-master/SEO/src/Http/Controllers/Admin/SettingController.php
@@ -13,6 +13,7 @@ use Rastventure\SEO\Services\SiteMap;
 use Rastventure\SEO\Http\Requests\Pages\Store;
 use Rastventure\SEO\Http\Requests\Pages\Edit;
 use Rastventure\SEO\Http\Requests\Pages\Update;
+use Rastventure\SEO\Models\Page;
 use Rastventure\SEO\Repositories\SettingRepository;
 use Webkul\Core\Repositories\CoreConfigRepository;
 use Webkul\Core\Tree;
@@ -97,12 +98,14 @@ class SettingController  extends \Webkul\Admin\Http\Controllers\Controller
         $data = [
             'records' => Setting::paginate(10),
             'model' => new Setting(),
+            'setting_total' => Setting::count(),
+            'page_total' => Page::count(),
             'sitemaps' => (new SiteMap())->all(),
             'metaTags' => isset($metaTags['webmaster_tools']) ? $metaTags['webmaster_tools'] : []
         ];
-
-
-        return view($this->_config['view'], ['data' => $data]);
+        return view('seo::admin.pages.settings.index', [
+            'data' => $data
+        ]);
     }
 
     /**
